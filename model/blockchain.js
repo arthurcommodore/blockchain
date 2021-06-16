e416615977f1812d5f43c3a07ae8863038cdf2bc
arthur <arhurdeveloper@gmail.com>
2021-06-16 04:54:45 -0300

    foi criado metodos para validar as blockchain correntes, dentro da classe Blockchain

--- model/blockchain.js
+++ model/blockchain.js
@@ -11,6 +11,27 @@ class Blockchain {
 
         return block
     }
+
+    isValidChain(chain) {
+        if(JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis))
+            return false
+
+        for(let i = 1; i < chain.length; i++) {
+            const block = chain[i] 
+            const lastBlock = chain[i - 1]
+
+            if(block.lasHash != lastBlock.hash || block.hash != Blockchain.blockHash(block))
+                return false;
+        }
+        return true
+    }
+
+    static blockHash(block) {
+        const {timestamp, lastHash, data} = block;
+        return Block.hash(timestamp, lastHash, data)
+    }
 }
+
+
 
 module.exports = Blockchain
