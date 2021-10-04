d83e87b9f2da5a3e64960855cc12e22b12b4f529
arthur <arhurdeveloper@gmail.com>
2021-10-04 01:24:13 -0300

    adionado configurações dos peers por meio do websocket

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
+const bodyParser = require('body-parser')
 const express = require('express')
 const Blockchain = require('./model/blockchain')
+const P2pServer = require('./P2pserver')
+
 const app = express()
 
-const HTTP_PORT =  3001
+const HTTP_PORT =  process.env.HTTP_PORT || 3022
 const bc = new Blockchain
+const p2pServer = new P2pServer(bc)
 
 app.use(express.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
 
 app.get('/chain', (req,res) => {
     res.json(bc.chain)
 })
 
 app.post('/mine', (req, res) => {
+    console.log(req.body.data)
     const block = bc.addBlock(req.body.data) 
     console.log(`New Block added: ${block.toString()}`)
 
@@ -19,3 +26,4 @@ app.post('/mine', (req, res) => {
 })
 
 app.listen(HTTP_PORT, () => console.log('work'))
+p2pServer.listen()
